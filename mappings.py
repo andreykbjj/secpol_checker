e7e0bd6cad6ea6a6672e180567597e596a4ed606
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-05 11:19:47 +0200

    Settings mappings + registry
    Work in progress

---
+++ mappings.py
@@ -0,0 +1,295 @@
+sid = {"S-1-0": "Null Authority",
+       "S-1-0-0": "Nobody",
+       "S-1-1": "World Authority",
+       "S-1-1-0": "Everyone",
+       "S-1-16-0": "Untrusted Mandatory Level",
+       "S-1-16-12288": "High Mandatory Level",
+       "S-1-16-16384": "System Mandatory Level",
+       "S-1-16-20480": "Protected Process Mandatory Level",
+       "S-1-16-28672": "Secure Process Mandatory Level",
+       "S-1-16-4096": "Low Mandatory Level",
+       "S-1-16-8192": "Medium Mandatory Level",
+       "S-1-16-8448": "Medium Plus Mandatory Level",
+       "S-1-2": "Local Authority",
+       "S-1-2-0": "Local",
+       "S-1-2-1": "Console Logon",
+       "S-1-3": "Creator Authority",
+       "S-1-3-0": "Creator Owner",
+       "S-1-3-1": "Creator Group",
+       "S-1-3-2": "Creator Owner Server",
+       "S-1-3-3": "Creator Group Server",
+       "S-1-3-4": "Owner Rights",
+       "S-1-4": "Non-unique Authority",
+       "S-1-5": "NT Authority",
+       "S-1-5-1": "Dialup",
+       "S-1-5-10": "Principal Self",
+       "S-1-5-11": "Authenticated Users",
+       "S-1-5-113": "Local account",
+       "S-1-5-114": "Local account and member of Administrators group",
+       "S-1-5-12": "Restricted Code",
+       "S-1-5-13": "Terminal Server User",
+       "S-1-5-14": "Remote Interactive Logon",
+       "S-1-5-15": "This Organization",
+       "S-1-5-17": "IIS_USRS",
+       "S-1-5-18": "System (or LocalSystem)",
+       "S-1-5-19": "NT Authority (LocalService)",
+       "S-1-5-2": "Network",
+       "S-1-5-20": "Network Service",
+       "S-1-5-21domain-498": "Enterprise Read-only Domain Controllers",
+       "S-1-5-21domain-500": "Administrator",
+       "S-1-5-21domain-501": "Guest",
+       "S-1-5-21domain-502": "KRBTGT",
+       "S-1-5-21domain-512": "Domain Admins",
+       "S-1-5-21domain-513": "Domain Users",
+       "S-1-5-21domain-514": "Domain Guests",
+       "S-1-5-21domain-515": "Domain Computers",
+       "S-1-5-21domain-516": "Domain Controllers",
+       "S-1-5-21domain-517": "Cert Publishers",
+       "S-1-5-21domain-520": "Group Policy Creator Owners",
+       "S-1-5-21domain-521": "Read-only Domain Controllers",
+       "S-1-5-21-domain-522": "Cloneable Domain Controllers",
+       "S-1-5-21domain-526": "Key Admins",
+       "S-1-5-21domain-527": "Enterprise Key Admins",
+       "S-1-5-21domain-553": "RAS and IAS Servers",
+       "S-1-5-21domain-571": "Allowed RODC Password Replication Group",
+       "S-1-5-21domain-572": "Denied RODC Password Replication Group",
+       "S-1-5-21root domain-518": "Schema Admins",
+       "S-1-5-21root domain-519": "Enterprise Admins",
+       "S-1-5-3": "Batch",
+       "S-1-5-32-544": "Administrators",
+       "S-1-5-32-545": "Users",
+       "S-1-5-32-546": "Guests",
+       "S-1-5-32-547": "Power Users",
+       "S-1-5-32-548": "Account Operators",
+       "S-1-5-32-549": "Server Operators",
+       "S-1-5-32-550": "Print Operators",
+       "S-1-5-32-551": "Backup Operators",
+       "S-1-5-32-552": "Replicators",
+       "S-1-5-32-554": "Pre-Windows 2000 Compatible Access",
+       "S-1-5-32-555": "Remote Desktop Users",
+       "S-1-5-32-556": "Network Configuration Operators",
+       "S-1-5-32-557": "Incoming Forest Trust Builders",
+       "S-1-5-32-558": "Performance Monitor Users",
+       "S-1-5-32-559": "Performance Log Users",
+       "S-1-5-32-560": "Windows Authorization Access Group",
+       "S-1-5-32-561": "Terminal Server License Servers",
+       "S-1-5-32-562": "Distributed COM Users",
+       "S-1-5-32-569": "Cryptographic Operators",
+       "S-1-5-32-573": "Event Log Readers",
+       "S-1-5-32-574": "Certificate Service DCOM Access",
+       "S-1-5-32-575": "RDS Remote Access Servers",
+       "S-1-5-32-576": "RDS Endpoint Servers",
+       "S-1-5-32-577": "RDS Management Servers",
+       "S-1-5-32-578": "Hyper-V Administrators",
+       "S-1-5-32-579": "Access Control Assistance Operators",
+       "S-1-5-32-580": "Remote Management Users",
+       "S-1-5-32-582": "Storage Replica Administrators",
+       "S-1-5-4": "Interactive",
+       "S-1-5-5-X-Y": "Logon Session",
+       "S-1-5-6": "Service",
+       "S-1-5-64-10": "NTLM Authentication",
+       "S-1-5-64-14": "SChannel Authentication",
+       "S-1-5-64-21": "Digest Authentication",
+       "S-1-5-7": "Anonymous Logon",
+       "S-1-5-8": "Proxy",
+       "S-1-5-80": "NT Service",
+       "S-1-5-80-0": "NT Services\\All Services",
+       "S-1-5-83-0": "NT Virtual Machine\\Virtual Machines",
+       "S-1-5-9": "Enterprise Domain Controllers",
+       "S-1-5-90-0": "Windows Manager\\Windows Manager Group",
+       "S-1-5-domain-500": "Administrator",
+       "S-1-5-domain-501": "Guest",
+       "S-1-5-domain-502": "krbtgt",
+       "S-1-5-domain-512": "Domain Admins",
+       "S-1-5-domain-513": "Domain Users",
+       "S-1-5-domain-514": "Domain Guests",
+       "S-1-5-domain-515": "Domain Computers",
+       "S-1-5-domain-516": "Domain Controllers",
+       "S-1-5-domain-517": "Cert Publishers",
+       "S-1-5-domain-520": "Group Policy Creator Owners",
+       "S-1-5-domain-553": "RAS and IAS Servers",
+       "S-1-5-root domain-518": "Schema Admins",
+       "S-1-5-root domain-519": "Enterprise Admins"
+       }
+
+password_policy = {
+    "MinimumPasswordAge": "Minimum password age",
+    "MaximumPasswordAge": "Maximum password age",
+    "MinimumPasswordLength": "Minimum password length",
+    "PasswordComplexity": "Password must meet complexity requirements",
+    "PasswordHistorySize": "Enforce password history",
+}
+
+lockout_policy = {
+    "LockoutBadCount": "Account lockout threshold",
+    "ResetLockoutCount": "Reset account lockout counter after",
+    "LockoutDuration": "Account lockout duration",
+}
+
+other_sec_options_configurations = {
+    "RequireLogonToChangePassword": "Require Logon To Change Password",
+    "ForceLogoffWhenHourExpire": "Force Logoff When Hour Expire",
+    "NewAdministratorName": "New Administrator Name",
+    "NewGuestName": "New Guest Name",
+    "ClearTextPassword": "Clear Text Password",
+    "LSAAnonymousNameLookup": "LSA Anonymous Name Lookup",
+    "EnableAdminAccount": "Enable Admin Account",
+    "EnableGuestAccount": "Enable Guest Account"
+}
+
+audit_dict = {
+    "AuditSystemEvents": "Audit System Events",
+    "AuditLogonEvents": "Audit Logon Events",
+    "AuditObjectAccess": "Audit Object Access",
+    "AuditPrivilegeUse": "Audit Privilege Use",
+    "AuditPolicyChange": "Audit Policy Change",
+    "AuditAccountManage": "Audit Account Manage",
+    "AuditProcessTracking": "Audit Process Tracking",
+    "AuditDSAccess": "Audit DS Access",
+    "AuditAccountLogon": "Audit Account Logon"
+}
+
+user_rights_assignment = {
+    'SeTrustedCredManAccessPrivilege': 'Access Credential Manager as a trusted caller',
+    'SeNetworkLogonRight': 'Access this computer from the network',
+    'SeTcbPrivilege': 'Act as part of the operating system',
+    'SeMachineAccountPrivilege': 'Add workstations to domain',
+    'SeIncreaseQuotaPrivilege': 'Adjust memory quotas for a process',
+    'SeInteractiveLogonRight': 'Allow log on locally',
+    'SeRemoteInteractiveLogonRight': 'Allow log on through Remote Desktop Services',
+    'SeBackupPrivilege': 'Back up files and directories',
+    'SeChangeNotifyPrivilege': 'Bypass traverse checking',
+    'SeSystemtimePrivilege': 'Change the system time',
+    'SeTimeZonePrivilege': 'Change the time zone',
+    'SeCreatePagefilePrivilege': 'Create a pagefile',
+    'SeCreateTokenPrivilege': 'Create a token object',
+    'SeCreateGlobalPrivilege': 'Create global objects',
+    'SeCreatePermanentPrivilege': 'Create permanent shared objects',
+    'SeCreateSymbolicLinkPrivilege': 'Create symbolic links',
+    'SeDebugPrivilege': 'Debug programs',
+    'SeDenyNetworkLogonRight': 'Deny access to this computer from the network',
+    'SeDenyBatchLogonRight': 'Deny log on as a batch job',
+    'SeDenyServiceLogonRight': 'Deny log on as a service',
+    'SeDenyInteractiveLogonRight': 'Deny log on locally',
+    'SeDenyRemoteInteractiveLogonRight': 'Deny log on through Remote Desktop Services',
+    'SeEnableDelegationPrivilege': 'Enable computer and user accounts to be trusted for delegation',
+    'SeRemoteShutdownPrivilege': 'Force shutdown from a remote system',
+    'SeAuditPrivilege': 'Generate security audits',
+    'SeImpersonatePrivilege': 'Impersonate a client after authentication',
+    'SeIncreaseWorkingSetPrivilege': 'Increase a process working set',
+    'SeIncreaseBasePriorityPrivilege': 'Increase scheduling priority',
+    'SeLoadDriverPrivilege': 'Load and unload device drivers',
+    'SeLockMemoryPrivilege': 'Lock pages in memory',
+    'SeBatchLogonRight': 'Log on as a batch job',
+    'SeServiceLogonRight': 'Log on as a service',
+    'SeSecurityPrivilege': 'Manage auditing and security log',
+    'SeRelabelPrivilege': 'Modify an object label',
+    'SeSystemEnvironmentPrivilege': 'Modify firmware environment values',
+    'SeManageVolumePrivilege': 'Perform volume maintenance tasks',
+    'SeProfileSingleProcessPrivilege': 'Profile single process',
+    'SeSystemProfilePrivilege': 'Profile system performance',
+    'SeUndockPrivilege': 'Remove computer from docking station',
+    'SeAssignPrimaryTokenPrivilege': 'Replace a process level token',
+    'SeRestorePrivilege': 'Restore files and directories',
+    'SeShutdownPrivilege': 'Shut down the system',
+    'SeSyncAgentPrivilege': 'Synchronize directory service data',
+    'SeTakeOwnershipPrivilege': 'Take ownership of files or other objects',
+    'SeDelegateSessionUserImpersonatePrivilege': 'Modify firmware environment values'
+}
+
+security_options = {
+    'CachedLogonsCount':
+        "Interactive logon: Number of previous logons to cache (in case domain controller is not available)",
+    'ForceUnlockLogon':
+        "Interactive logon: Require Domain Controller Authentication to unlock workstation",
+    'PasswordExpiryWarning=4,7':
+        "Interactive logon: Prompt user to change password before expiration",
+    'ScRemoveOption':
+        "Interactive logon: Smart card removal behavior",
+    'ConsentPromptBehaviorAdmin':
+        "User Account Control: Behavior of the elevation prompt for administrators in Admin Approval Mode",
+    'ConsentPromptBehaviorUser':
+        "User Account Control: Behavior of the elevation prompt for standard users",
+    'DisableCAD':
+        "Interactive logon: Do not require CTRL+ALT+DEL",
+    'DontDisplayLastUserName':
+        "Interactive logon: Do not display last user name",
+    'EnableInstallerDetection':
+        "User Account Control: Detect application installations and prompt for elevation",
+    'EnableLUA':
+        "User Account Control: Run all administrators in Admin Approval Mode",
+    'EnableSecureUIAPaths':
+        "User Account Control: Only elevate UIAccess applications that are installed in secure locations",
+    'EnableUIADesktopToggle':
+        "User Account Control: Allow UIAccess applications to prompt for elevation without using the secure desktop",
+    'EnableVirtualization':
+        "User Account Control: Virtualize file and registry write failures to per-user locations",
+    'FilterAdministratorToken':
+        "User Account Control: Admin Approval Mode for the Built-in Administrator account",
+    'LegalNoticeCaption':
+        "Interactive logon: Message title for users attempting to log on",
+    'LegalNoticeText':
+        "Interactive logon: Message text for users attempting to log on",
+    'PromptOnSecureDesktop':
+        "User Account Control: Switch to the secure desktop when prompting for elevation",
+    'ShutdownWithoutLogon':
+        "Shutdown: Allow system to be shut down without having to log on",
+    'CrashOnAuditFail':
+        "Audit: Shut down system immediately if unable to log security audits",
+    'DisableDomainCreds':
+        "Network access: Do not allow storage of passwords and credentials for network authentication",
+    'ForceGuest':
+        "Network access: Sharing and security model for local accounts",
+    'LimitBlankPasswordUse':
+        "Accounts: Limit local account use of blank passwords to console logon only",
+    'NoLMHash':
+        "Network security: Do not store LAN Manager hash value on next password change",
+    'RestrictAnonymousSAM':
+        "Network access: Do not allow anonymous enumeration of SAM accounts",
+    'ObCaseInsensitive':
+        "System objects: Require case insensitivity for nonWindows subsystems",
+    'ProtectionMode':
+        "System objects: Strengthen default permissions of internal system objects (e.g. Symbolic Links)",
+    'optional,':
+        "System settings: Optional subsystems",
+    'AutoDisconnect5':
+        "Microsoft network server: Amount of idle time required before suspending session",
+    'EnableForcedLogOff':
+        "Microsoft network server: Disconnect clients when logon hours expire",
+    'EnableSecuritySignature':
+        "Microsoft network client: Digitally sign communications (if server agrees)",
+    'NullSessionPipes,':
+        "Network access: Named Pipes that can be accessed anonymously",
+    'RequireSecuritySignature':
+        "Microsoft network client: Digitally sign communications (always)",
+    'RestrictNullSessAccess':
+        "Network access: Restrict anonymous access to Named Pipes and Shares",
+    'EnablePlainTextPassword':
+        "Microsoft network client: Send unencrypted password to third-party SMB servers",
+    #     'EnableSecuritySignature':
+    # "Microsoft network server: Digitally sign communications (if client agrees)",
+    #     'RequireSecuritySignature':
+    # "Microsoft network server: Digitally sign communications (always)",
+    'LDAPClientIntegrity':
+        "Network security: LDAP client signing requirements",
+    'DisablePasswordChange':
+        "Domain member: Disable machine account password changes",
+    'RequireSignOrSeal':
+        "Domain member: Digitally encrypt or sign secure channel data (always)",
+    'RequireStrongKey':
+        "Domain member: Require strong (Windows 2000 or later) session key",
+    'SealSecureChannel':
+        "Domain member: Digitally encrypt secure channel data (when possible)",
+    'SignSecureChannel':
+        "Domain member: Digitally sign secure channel data (when possible)"
+}
+
+
+# Continue..
+reg_keys = {
+    '0': '0-Enabled',
+    '2': 'Third Option',
+    '1': '1-Enabled',
+    '4': '4-Enabled',
+    '3': '3-Disabled',
+    '7': '7-No message'}
