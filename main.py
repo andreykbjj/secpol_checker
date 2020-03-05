fe602caa52d3beaec032f57216c50eb011574721
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-05 11:18:44 +0200

    Work in progress

---
+++ main.py
@@ -0,0 +1,189 @@
+from mappings import sid, password_policy, lockout_policy, other_sec_options_configurations, user_rights_assignment,\
+    security_options, audit_dict, reg_keys
+from termcolor import colored
+
+sec_edit_file = 'this_pc.txt'
+
+
+def get_line_number(phrase):
+    with open(f'{sec_edit_file}', 'r', encoding='utf16') as f:
+        for index, line in enumerate(f, 1):
+            if phrase in line:
+                return index
+
+
+def privilege_rights_to_dict():
+    try:
+        raw_local_p_user_right_dict = dict()
+        fine_user_right_dict = dict()
+
+        start = get_line_number(phrase="[Privilege Rights]")
+        end = get_line_number(phrase="[Version]") - 1
+
+        with open(f'{sec_edit_file}', 'r', encoding='utf16') as user_rights:
+            for line in user_rights.readlines()[start:end]:
+                line = line.replace(' = *', ':').replace('*', '').replace(' = ', ':').strip('\n').split(':')
+
+                configs = line[0]
+                sids = line[1].split(',')
+                contains = list()
+                for s in sids:
+                    if sid.get(s) is None:
+                        contains.append(s)  # Translate SID's
+                    else:
+                        contains.append(sid.get(s))
+                new_line_dict = {configs: contains}
+                raw_local_p_user_right_dict.update(new_line_dict)
+
+        for conf, value in raw_local_p_user_right_dict.items():
+            latest = {user_rights_assignment.get(conf): value}
+            fine_user_right_dict.update(latest)
+
+        # for k, v in fine_local_p_user_right_dict.items():
+        #     print(k, *v)
+        print(colored(fine_user_right_dict, 'blue'))
+
+        return fine_user_right_dict
+
+    except IndexError as index_error:
+        print(f'Error: {index_error}')
+
+
+def security_options_registry_values_to_dict():  # Map Registry Values in reg_keys dict
+    try:
+
+        fine_security_options_dict = dict()
+
+        start = get_line_number(phrase="[Registry Values]")
+        end = get_line_number(phrase="[Privilege Rights]") - 1
+
+        with open(f'{sec_edit_file}', 'r', encoding='utf16') as sec_options:
+            for line in sec_options.readlines()[start:end]:
+                line = line.strip('\n').replace('=', ':').split(':')
+                configs = line[0]
+                status = line[1].split(',')
+                for k in security_options.keys():
+                    if configs.__contains__(k):
+                        index = configs.find(k)
+                        if status[0] in reg_keys.keys():
+                            status = [reg_keys.get(status[0]), reg_keys.get(status[1])]
+                            mapped = {security_options.get(configs[index:]): status}
+                            fine_security_options_dict.update(mapped)
+
+        # for keys, values in fine_security_options_dict.items():
+        #     print(keys, *values)
+        print(colored(fine_security_options_dict, 'red'))
+
+        return fine_security_options_dict
+
+    except IndexError as index_error:
+        print(f'Error: {index_error}')
+
+
+def other_sec_options_policy_to_dict():
+    try:
+
+        raw_other_sec_options_dict = dict()
+
+        start = get_line_number(phrase="LockoutDuration")
+        end = get_line_number(phrase="[Event Audit]") - 1
+
+        with open(f'{sec_edit_file}', 'r', encoding='utf16') as sys_access:
+            for line in sys_access.readlines()[start:end]:
+                line = line.strip('\n').replace(' = ', ':').split(':')
+                if line[0] in other_sec_options_configurations.keys():
+                    keys = other_sec_options_configurations.get(line[0])
+                    vals = line[1]
+                    new_dict = {keys: vals}
+                    raw_other_sec_options_dict.update(new_dict)
+
+        print(colored(raw_other_sec_options_dict, 'red'))
+
+        return
+
+    except IndexError as index_error:
+        print(f'Error: {index_error}')
+
+
+def password_policy_to_dict():
+    try:
+
+        raw_password_policy_dict = dict()
+
+        start = get_line_number(phrase="[System Access]")
+        end = get_line_number(phrase="LockoutBadCount") - 1
+
+        with open(f'{sec_edit_file}', 'r', encoding='utf16') as sys_access:
+            for line in sys_access.readlines()[start:end]:
+                line = line.strip('\n').replace(' = ', ':').split(':')
+                if line[0] in password_policy.keys():
+                    keys = password_policy.get(line[0])
+                    vals = line[1]
+                    new_dict = {keys: int(vals)}
+                    raw_password_policy_dict.update(new_dict)
+
+        print(colored(raw_password_policy_dict, 'blue'))
+
+        return
+
+    except IndexError as index_error:
+        print(f'Error: {index_error}')
+
+
+def lockout_policy_to_dict():
+    try:
+
+        raw_lockout_policy_dict = dict()
+
+        start = get_line_number(phrase="PasswordHistorySize")
+        end = get_line_number(phrase="RequireLogonToChangePassword") - 1
+
+        with open(f'{sec_edit_file}', 'r', encoding='utf16') as sys_access:
+            for line in sys_access.readlines()[start:end]:
+                line = line.strip('\n').replace(' = ', ':').split(':')
+                if line[0] in lockout_policy.keys():
+                    keys = lockout_policy.get(line[0])
+                    vals = line[1]
+                    new_dict = {keys: int(vals)}
+                    raw_lockout_policy_dict.update(new_dict)
+
+        print(colored(raw_lockout_policy_dict, 'blue'))
+
+        return
+
+    except IndexError as index_error:
+        print(f'Error: {index_error}')
+
+
+def event_audit():
+    try:
+
+        raw_local_p_user_right_dict = dict()
+
+        start = get_line_number(phrase="[Event Audit]")
+        end = get_line_number(phrase="[Registry Values]") - 1
+
+        with open(f'{sec_edit_file}', 'r', encoding='utf16') as audit:
+            for line in audit.readlines()[start:end]:
+                line = line.strip('\n').replace(' = ', ':').split(':')
+                if line[0] in audit_dict.keys():
+                    keys = audit_dict.get(line[0])
+                    vals = line[1]
+                    new_dict = {keys: int(vals)}
+                    raw_local_p_user_right_dict.update(new_dict)
+
+            print(colored(raw_local_p_user_right_dict, 'red'))
+
+            return
+
+    except IndexError as index_error:
+        print(f'Error: {index_error}')
+
+
+password_policy_to_dict()
+lockout_policy_to_dict()
+privilege_rights_to_dict()
+
+event_audit()
+security_options_registry_values_to_dict()
+other_sec_options_policy_to_dict()
