f2973f586b91df45850e18812d85f571e9993baf
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-05 15:04:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from mappings import sid, password_policy, lockout_policy, other_sec_options_configurations, user_rights_assignment,\
-    security_options, audit_dict, reg_keys
+from mappings import sid, local_policy, reg_keys
 from termcolor import colored
 
 sec_edit_file = 'this_pc.txt'
@@ -36,7 +35,7 @@ def privilege_rights_to_dict():
                 raw_local_p_user_right_dict.update(new_line_dict)
 
         for conf, value in raw_local_p_user_right_dict.items():
-            latest = {user_rights_assignment.get(conf): value}
+            latest = {local_policy['user_rights_assignment'].get(conf): value}
             fine_user_right_dict.update(latest)
 
         # for k, v in fine_local_p_user_right_dict.items():
@@ -62,12 +61,12 @@ def security_options_registry_values_to_dict():  # Map Registry Values in reg_ke
                 line = line.strip('\n').replace('=', ':').split(':')
                 configs = line[0]
                 status = line[1].split(',')
-                for k in security_options.keys():
+                for k in local_policy['security_options'].keys():
                     if configs.__contains__(k):
                         index = configs.find(k)
-                        if status[0] in reg_keys.keys():
-                            status = [reg_keys.get(status[0]), reg_keys.get(status[1])]
-                            mapped = {security_options.get(configs[index:]): status}
+                        if status[0] in reg_keys['Security Options'].keys():
+                            status = [reg_keys['Security Options'].get(status[0]), reg_keys['Security Options'].get(status[1])]
+                            mapped = {local_policy['security_options'].get(configs[index:]): status}
                             fine_security_options_dict.update(mapped)
 
         # for keys, values in fine_security_options_dict.items():
@@ -91,8 +90,8 @@ def other_sec_options_policy_to_dict():
         with open(f'{sec_edit_file}', 'r', encoding='utf16') as sys_access:
             for line in sys_access.readlines()[start:end]:
                 line = line.strip('\n').replace(' = ', ':').split(':')
-                if line[0] in other_sec_options_configurations.keys():
-                    keys = other_sec_options_configurations.get(line[0])
+                if line[0] in local_policy['other_sec_options_configurations'].keys():
+                    keys = local_policy['other_sec_options_configurations'].get(line[0])
                     vals = line[1]
                     new_dict = {keys: vals}
                     raw_other_sec_options_dict.update(new_dict)
@@ -116,8 +115,8 @@ def password_policy_to_dict():
         with open(f'{sec_edit_file}', 'r', encoding='utf16') as sys_access:
             for line in sys_access.readlines()[start:end]:
                 line = line.strip('\n').replace(' = ', ':').split(':')
-                if line[0] in password_policy.keys():
-                    keys = password_policy.get(line[0])
+                if line[0] in local_policy['password_policy'].keys():
+                    keys = local_policy['password_policy'].get(line[0])
                     vals = line[1]
                     new_dict = {keys: int(vals)}
                     raw_password_policy_dict.update(new_dict)
@@ -141,8 +140,8 @@ def lockout_policy_to_dict():
         with open(f'{sec_edit_file}', 'r', encoding='utf16') as sys_access:
             for line in sys_access.readlines()[start:end]:
                 line = line.strip('\n').replace(' = ', ':').split(':')
-                if line[0] in lockout_policy.keys():
-                    keys = lockout_policy.get(line[0])
+                if line[0] in local_policy['lockout_policy'].keys():
+                    keys = local_policy['lockout_policy'].get(line[0])
                     vals = line[1]
                     new_dict = {keys: int(vals)}
                     raw_lockout_policy_dict.update(new_dict)
@@ -166,13 +165,12 @@ def event_audit():
         with open(f'{sec_edit_file}', 'r', encoding='utf16') as audit:
             for line in audit.readlines()[start:end]:
                 line = line.strip('\n').replace(' = ', ':').split(':')
-                if line[0] in audit_dict.keys():
-                    keys = audit_dict.get(line[0])
+                if line[0] in local_policy['audit_dict'].keys():
+                    keys = local_policy['audit_dict'].get(line[0])
                     vals = line[1]
-                    new_dict = {keys: int(vals)}
+                    new_dict = {keys: reg_keys['Audit'].get(int(vals))}
                     raw_local_p_user_right_dict.update(new_dict)
-
-            print(colored(raw_local_p_user_right_dict, 'red'))
+            print(colored(raw_local_p_user_right_dict, 'blue'))
 
             return
 
@@ -182,8 +180,7 @@ def event_audit():
 
 password_policy_to_dict()
 lockout_policy_to_dict()
-privilege_rights_to_dict()
-
 event_audit()
-security_options_registry_values_to_dict()
+privilege_rights_to_dict()
 other_sec_options_policy_to_dict()
+security_options_registry_values_to_dict()
