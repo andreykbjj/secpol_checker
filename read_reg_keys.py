4dc6f02eefd1a4bd5c10419927c830256b7bafb1
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-10 16:31:04 +0200

    Update read_reg_keys.py

--- read_reg_keys.py
+++ read_reg_keys.py
@@ -1,6 +1,5 @@
 import codecs
 
-regFile = codecs.open("reg_keys.reg", encoding="utf8")
-reg = regFile.read()
-regFile.close()
-print(reg.replace('Windows Registry Editor Version 5.00', '').strip('\n'))
+with codecs.open("reg_keys.reg", encoding="utf8") as regFile:
+    reg = regFile.read()
+    print(reg.replace('Windows Registry Editor Version 5.00', '').strip('\n'))
