63e7e33f70240c5051bca5f2800e14c278f13435
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-12 17:00:06 +0200

    Delete read_reg_keys.py

--- read_reg_keys.py
+++
@@ -1,5 +0,0 @@
-import codecs
-
-with codecs.open("reg_keys.reg", encoding="utf8") as regFile:
-    reg = regFile.read()
-    print(reg.replace('Windows Registry Editor Version 5.00', '').strip('\n'))
