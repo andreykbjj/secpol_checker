20f17f30c90df1e2f4a0a50c286ef768e68a5276
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-12 16:59:32 +0200

    Create transcript_checker.py

---
+++ transcript_checker.py
@@ -0,0 +1,23 @@
+from reg_value_names import reg_values
+
+new_list = list()
+with open('transcript.txt', 'r', encoding='utf8') as transcript:
+    for line in transcript.readlines():
+        new_list.append(list(filter(None, line.strip('\n').replace(' ', '').split(':'))))
+
+
+super_new = list(filter(None, new_list))
+new_dict = dict()
+
+for i in super_new:
+    if len(i) > 1:
+        # print(i)
+        keys = i[0]
+        values = i[1]
+        test = {keys: values}
+        new_dict.update(test)
+
+for k, v in new_dict.items():
+    for reg in reg_values:
+        if k == reg:
+            print(k, v)
