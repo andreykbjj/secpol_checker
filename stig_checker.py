502ff585339461a44ae87688140369c7debfb6a2
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-13 14:03:17 +0200

    Update stig_checker.py

--- stig_checker.py
+++ stig_checker.py
@@ -1,7 +1,7 @@
 from termcolor import colored
 import stig
 
-cats = stig.win_server_2012.get('stig')['findings']
+cats = stig.win_server_2012_r2.get('stig')['findings']
 
 insides = [
     "checkid"
@@ -17,29 +17,27 @@ insides = [
     "version"
 ]
 
-main_dict = dict()
-new_listing = list()
 
-for k, v in cats.items():
-    if v.get('checktext').strip('\n').__contains__(':'):
-        testing = list()
-        splitted = list()
-        # print(colored("*" * 100, 'yellow'))
-
-        testing.append(v.get('checktext').strip('\n'))
-
-        for item in testing:
-            # print(item.split('\n'))
-            for i in item.split('\n'):
-                if i.__contains__('Registry Path'):
-                    keys = 'Get-ItemProperty -Path "HKLM:' + i.split(': ')[1].lstrip(' ') + '"'
-                    new_listing.append(keys)
-                    # print(keys)
-                elif i.__contains__('Value Name'):
-                    values = i.split(': ')[1]
-                    values = ' | fl ' + values + '\n'
-                    new_listing.append(values)
-                    # print(values)
-new_list = (list(filter(None, new_listing)))
-print(''.join(new_list).strip('\n'))
+def get_item_property():
+    main_dict = dict()
+    new_listing = list()
+    
+    for k, v in cats.items():
+        if v.get('checktext').strip('\n').__contains__(':'):
+            testing = list()
+            
+            # print(colored("*" * 100, 'yellow'))
+            testing.append(v.get('checktext').strip('\n'))
+    
+            for item in testing:
+                for i in item.split('\n'):
+                    if i.__contains__('Registry Path'):
+                        keys = 'Get-ItemProperty -Path "HKLM:' + i.split(': ')[1].lstrip(' ') + '"'
+                        new_listing.append(keys)
+                    elif i.__contains__('Value Name'):
+                        values = i.split(': ')[1]
+                        values = ' | fl ' + values + '\n'
+                        new_listing.append(values)
+    new_list = (list(filter(None, new_listing)))
+    print(''.join(new_list).strip('\n'))
 
