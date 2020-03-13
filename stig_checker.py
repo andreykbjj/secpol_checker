650559594457acdf67ea447a0099ecb7aec53a60
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-03-14 01:09:13 +0200

    Update stig_checker.py

--- stig_checker.py
+++ stig_checker.py
@@ -1,46 +1,8 @@
 from termcolor import colored
+
 import stig
 
 cats = stig.win_server_2012_r2.get('stig')['findings']
-
-insides = [
-    "checkid"
-    "checktext"
-    "description"
-    "fixid"
-    "fixtext"
-    "iacontrols"
-    "id"
-    "ruleID"
-    "severity"
-    "title"
-    "version"
-]
-
-
-def get_item_property():
-    new_listing = list()
-    for k, v in cats.items():
-
-        if v.get('checktext').strip('\n').__contains__(':'):
-            raw_list = list()
-            # print(colored("*" * 100, 'yellow'))
-            raw_list.append(v.get('checktext').strip('\n'))
-
-            for item in raw_list:
-                for i in item.split('\n'):
-                    if i.__contains__('Registry Path'):
-                        keys = 'Get-ItemProperty -Path "HKLM:' + i.split(': ')[1].lstrip(' ') + '"'
-                        new_listing.append(keys)
-                    elif i.__contains__('Value Name'):
-                        values = i.split(': ')[1]
-                        values = ' | fl ' + values + '\n'
-                        new_listing.append(values)
-
-    new_list = (list(filter(None, new_listing)))
-
-    # print(''.join(new_list).strip('\n'))
-    return ''.join(new_list).strip('\n')
 
 
 def read_pulled_txt(file_name: str):
@@ -49,6 +11,7 @@ def read_pulled_txt(file_name: str):
     with open(file_name, 'r') as pulled:
         for line in pulled.readlines():
             only_configs.append(line.strip('\n').split(' : '))
+    # print(only_configs)
     for item in only_configs:
         if item != ['']:
             if len(item) > 1:
@@ -57,6 +20,7 @@ def read_pulled_txt(file_name: str):
                 config_dict = {key: value}
                 pulled_configs_dict.update(config_dict)
 
+    # print(pulled_configs_dict)
     return pulled_configs_dict
 
 
@@ -84,11 +48,15 @@ def combine_checks():
 combine_checks()
 text = read_pulled_txt(file_name='pulled.txt')
 
-translate = {'0': 'Disabled', '1': 'Enabled'}
+values = {'0': 'Disabled', '1': 'Enabled'}
 
 # Test Loop
+
 for k in combine_checks().keys():
     if k in text.keys():
-        # print(combine_checks().get(k), values.get(text.get(k)))
-        print(combine_checks().get(k), translate.get(text.get(k)))
-        print(colored("*" * len(combine_checks().get(k)), 'yellow'))
+        if values.get(text.get(k)) is None:
+            print(combine_checks().get(k), text.get(k))
+            print(colored("*" * len(combine_checks().get(k)), 'yellow'))
+        else:
+            print(combine_checks().get(k), '-', colored(values.get(text.get(k)), 'red'))
+            print(colored("*" * len(combine_checks().get(k)), 'yellow'))
